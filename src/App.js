e26f8148084d193cca0d74f52beba7b490b92d4e
James David West <thefakejddubs@gmail.com>
2024-01-18 13:31:24 -0600

    adding app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const [count, setCount] = useState(0); // (stateVariable, setterFunction)
 
-export default App;
+	const handleButtonClick = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<> 
+			<h1>Hello</h1>
+			<div>This is amazing!</div>
+			<button className="btn-secondary" onClick={handleButtonClick}>
+				Click to Increment Count
+			</button>
+			<div>Count: {count}</div>
+		</>
+	);
+};
